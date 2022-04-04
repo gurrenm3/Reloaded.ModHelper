36702d2afe3403906887e154e984ab24e2a824ee
Thomas Margraff <thomaspmar@protonmail.com>
2022-04-03 23:29:34 -0700

    Improved ActioRef delegates

--- Reloaded.ModHelper.Interfaces/Events/ActionRef.cs
+++ Reloaded.ModHelper.Interfaces/Events/ActionRef.cs
@@ -9,35 +9,28 @@
 
 
     /// <summary>
-    /// A delegate type with a referenceable argument
+    /// A delegate type with referenceable arguments.
     /// </summary>
     /// <typeparam name="T1"></typeparam>
     /// <typeparam name="T2"></typeparam>
     /// <param name="item1"></param>
     /// <param name="item2"></param>
-    public delegate void ActionRef<T1, T2>(ref T1 item1, ref T2 item2);
+    public delegate void ActionRef<T1, T2>(ref T1 item1, T2 item2);
 
     /// <summary>
-    /// A delegate type with a referenceable argument
+    /// A delegate type with referenceable arguments.
     /// </summary>
     /// <typeparam name="T1"></typeparam>
     /// <typeparam name="T2"></typeparam>
     /// <param name="item1"></param>
     /// <param name="item2"></param>
-    public delegate void ActionRef1<T1, T2>(ref T1 item1, T2 item2);
+    public delegate void ActionRef1<T1, T2>(ref T1 item1, ref T2 item2);
 
-    /// <summary>
-    /// A delegate type with a referenceable argument
-    /// </summary>
-    /// <typeparam name="T1"></typeparam>
-    /// <typeparam name="T2"></typeparam>
-    /// <param name="item1"></param>
-    /// <param name="item2"></param>
-    public delegate void ActionRef2<T1, T2>(T1 item1, ref T2 item2);
+
 
 
     /// <summary>
-    /// A delegate type with a referenceable argument
+    /// A delegate type with referenceable arguments.
     /// </summary>
     /// <typeparam name="T1"></typeparam>
     /// <typeparam name="T2"></typeparam>
@@ -45,10 +38,10 @@
     /// <param name="item1"></param>
     /// <param name="item2"></param>
     /// <param name="item3"></param>
-    public delegate void ActionRef<T1, T2, T3>(ref T1 item1, ref T2 item2, ref T3 item3);
+    public delegate void ActionRef<T1, T2, T3>(ref T1 item1, T2 item2, T3 item3);
 
     /// <summary>
-    /// A delegate type with a referenceable argument
+    /// A delegate type with referenceable arguments.
     /// </summary>
     /// <typeparam name="T1"></typeparam>
     /// <typeparam name="T2"></typeparam>
@@ -56,10 +49,10 @@
     /// <param name="item1"></param>
     /// <param name="item2"></param>
     /// <param name="item3"></param>
-    public delegate void ActionRef1<T1, T2, T3>(ref T1 item1, T2 item2, T3 item3);
+    public delegate void ActionRef1<T1, T2, T3>(ref T1 item1, ref T2 item2, T3 item3);
 
     /// <summary>
-    /// A delegate type with a referenceable argument
+    /// A delegate type with referenceable arguments.
     /// </summary>
     /// <typeparam name="T1"></typeparam>
     /// <typeparam name="T2"></typeparam>
@@ -67,49 +60,136 @@
     /// <param name="item1"></param>
     /// <param name="item2"></param>
     /// <param name="item3"></param>
-    public delegate void ActionRef2<T1, T2, T3>(T1 item1, ref T2 item2, T3 item3);
+    public delegate void ActionRef2<T1, T2, T3>(ref T1 item1, ref T2 item2, ref T3 item3);
+
+
 
     /// <summary>
-    /// A delegate type with a referenceable argument
+    /// A delegate type with referenceable arguments.
     /// </summary>
     /// <typeparam name="T1"></typeparam>
     /// <typeparam name="T2"></typeparam>
     /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
     /// <param name="item1"></param>
     /// <param name="item2"></param>
     /// <param name="item3"></param>
-    public delegate void ActionRef3<T1, T2, T3>(T1 item1, T2 item2, ref T3 item3);
+    /// <param name="item4"></param>
+    public delegate void ActionRef<T1, T2, T3, T4>(ref T1 item1, T2 item2, T3 item3, T4 item4);
 
     /// <summary>
-    /// A delegate type with a referenceable argument
+    /// A delegate type with referenceable arguments.
     /// </summary>
     /// <typeparam name="T1"></typeparam>
     /// <typeparam name="T2"></typeparam>
     /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
     /// <param name="item1"></param>
     /// <param name="item2"></param>
     /// <param name="item3"></param>
-    public delegate void ActionRef4<T1, T2, T3>(ref T1 item1, ref T2 item2, T3 item3);
+    /// <param name="item4"></param>
+    public delegate void ActionRef1<T1, T2, T3, T4>(ref T1 item1, ref T2 item2, T3 item3, T4 item4);
 
     /// <summary>
-    /// A delegate type with a referenceable argument
+    /// A delegate type with referenceable arguments.
     /// </summary>
     /// <typeparam name="T1"></typeparam>
     /// <typeparam name="T2"></typeparam>
     /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
     /// <param name="item1"></param>
     /// <param name="item2"></param>
     /// <param name="item3"></param>
-    public delegate void ActionRef5<T1, T2, T3>(T1 item1, ref T2 item2, ref T3 item3);
+    /// <param name="item4"></param>
+    public delegate void ActionRef2<T1, T2, T3, T4>(ref T1 item1, ref T2 item2, ref T3 item3, T4 item4);
 
     /// <summary>
-    /// A delegate type with a referenceable argument
+    /// A delegate type with referenceable arguments.
     /// </summary>
     /// <typeparam name="T1"></typeparam>
     /// <typeparam name="T2"></typeparam>
     /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
     /// <param name="item1"></param>
     /// <param name="item2"></param>
     /// <param name="item3"></param>
-    public delegate void ActionRef6<T1, T2, T3>(ref T1 item1, T2 item2, ref T3 item3);
+    /// <param name="item4"></param>
+    public delegate void ActionRef3<T1, T2, T3, T4>(ref T1 item1, ref T2 item2, ref T3 item3, ref T4 item4);
+
+
+
+    /// <summary>
+    /// A delegate type with referenceable arguments.
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <param name="item1"></param>
+    /// <param name="item2"></param>
+    /// <param name="item3"></param>
+    /// <param name="item4"></param>
+    /// <param name="item5"></param>
+    public delegate void ActionRef<T1, T2, T3, T4, T5>(ref T1 item1, T2 item2, T3 item3, T4 item4, T5 item5);
+
+    /// <summary>
+    /// A delegate type with referenceable arguments.
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <param name="item1"></param>
+    /// <param name="item2"></param>
+    /// <param name="item3"></param>
+    /// <param name="item4"></param>
+    /// <param name="item5"></param>
+    public delegate void ActionRef1<T1, T2, T3, T4, T5>(ref T1 item1, ref T2 item2, T3 item3, T4 item4, T5 item5);
+
+    /// <summary>
+    /// A delegate type with referenceable arguments.
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <param name="item1"></param>
+    /// <param name="item2"></param>
+    /// <param name="item3"></param>
+    /// <param name="item4"></param>
+    /// <param name="item5"></param>
+    public delegate void ActionRef2<T1, T2, T3, T4, T5>(ref T1 item1, ref T2 item2, ref T3 item3, T4 item4, T5 item5);
+
+    /// <summary>
+    /// A delegate type with referenceable arguments.
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <param name="item1"></param>
+    /// <param name="item2"></param>
+    /// <param name="item3"></param>
+    /// <param name="item4"></param>
+    /// <param name="item5"></param>
+    public delegate void ActionRef3<T1, T2, T3, T4, T5>(ref T1 item1, ref T2 item2, ref T3 item3, ref T4 item4, T5 item5);
+
+    /// <summary>
+    /// A delegate type with referenceable arguments.
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <param name="item1"></param>
+    /// <param name="item2"></param>
+    /// <param name="item3"></param>
+    /// <param name="item4"></param>
+    /// <param name="item5"></param>
+    public delegate void ActionRef4<T1, T2, T3, T4, T5>(ref T1 item1, ref T2 item2, ref T3 item3, ref T4 item4, ref T5 item5);
 }
