48324f31d7ccb319909a6e5e060b47f00cc6cac5
Thomas Margraff <thomaspmar@protonmail.com>
2022-06-26 08:03:29 -0700

    Added Read/Write object in memory

--- Reloaded.ModHelper/Api/Memory/MemoryManager.cs
+++ Reloaded.ModHelper/Api/Memory/MemoryManager.cs
@@ -32,6 +32,92 @@ namespace Reloaded.ModHelper.Api
                 RegisterConverter(item);
         }
 
+        /// <summary>
+        /// Reads an object in memory.
+        /// </summary>
+        /// <typeparam name="T">The type of object to be read.</typeparam>
+        /// <param name="address">The base address of this object.</param>
+        /// <returns></returns>
+        public T GetObject<T>(long address)
+        {
+            var value = GetObject(typeof(T), address);
+            return value == null ? default(T) : (T)value;
+        }
+
+        /// <summary>
+        /// Reads an object in memory.
+        /// </summary>
+        /// <param name="objectType">The type of object to be read.</param>
+        /// <param name="address">The base address of this object.</param>
+        /// <returns></returns>
+        /// <exception cref="NotSupportedException"></exception>
+        public object GetObject(Type objectType, long address)
+        {
+            var converter = GetConverter(objectType);
+            if (converter == null)
+                throw new NotSupportedException($"Cannot convert object of type {objectType.Name}. Type is not supported." +
+                    $" Consider creating your own {nameof(IMemoryConverter)} to add support for this.");
+
+            return converter.GetValue(objectType, address);
+        }
+
+        /// <summary>
+        /// Sets an object in memory at the provided address.
+        /// </summary>
+        /// <param name="address">The base address of the object.</param>
+        /// <param name="valueToSet">The new value to set at the address.</param>
+        public void SetObject(long address, object valueToSet)
+        {
+            var converter = GetConverter(valueToSet.GetType());
+            if (converter == null)
+                throw new NotSupportedException($"Cannot convert object of type {valueToSet.GetType().Name}. Type is not supported." +
+                    $" Consider creating your own {nameof(IMemoryConverter)} to add support for this.");
+
+            converter.SetValue(address, valueToSet);
+        }
+
+
+        /// <summary>
+        /// Returns the converter that can convert an object of type T.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public IMemoryConverter GetConverter<T>()
+        {
+            return GetConverter(typeof(T));
+        }
+
+        /// <summary>
+        /// Returns the converter that can convert an object of the provided type.
+        /// Will return null if converter does not exist.
+        /// </summary>
+        /// <param name="converterType"></param>
+        /// <returns></returns>
+        public IMemoryConverter GetConverter(Type converterType)
+        {
+            return converters.FirstOrDefault(c => c.CanConvert(converterType));
+        }
+
+        /// <summary>
+        /// Returns whether or not this type of object can be converted.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public bool CanConvert<T>()
+        {
+            return CanConvert(typeof(T));
+        }
+
+        /// <summary>
+        /// Returns whether or not this type of object can be converted.
+        /// </summary>
+        /// <param name="objectType"></param>
+        /// <returns></returns>
+        public bool CanConvert(Type objectType)
+        {
+            return GetConverter(objectType) != null;
+        }
+
 
         /// <summary>
         /// Returns whether or not this converter is already registered.
