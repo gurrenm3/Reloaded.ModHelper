696ff23ed9bc4022ca84ff7c8127291b1f4c7a21
Thomas Margraff <thomaspmar@protonmail.com>
2022-09-11 21:11:36 -0700

    Added DictionaryExtensions AddOrUpdate

--- Reloaded.ModHelper/Api/Memory/Signature.cs
+++ Reloaded.ModHelper/Api/Memory/Signature.cs
@@ -1,5 +1,6 @@
 ﻿using Reloaded.Memory.Sigscan;
 using Reloaded.Memory.Sigscan.Structs;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 
---
+++ Reloaded.ModHelper/Extensions/System/DictionaryExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Reloaded.ModHelper
+{
+    /// <summary>
+    /// Extension methods for Dictionaries.
+    /// </summary>
+    public static class DictionaryExtensions
+    {
+        /// <summary>
+        /// Allows you to Add or Update a KeyValuePair in this dictionary. If it doesn't already exist the pair will be
+        /// added, otherwise the existing one will be updated with the provided <paramref name="value"/>.
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="dictionary"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public static void AddOrUpdate<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue value)
+        {
+            if (!dictionary.TryAdd(key, value))
+                dictionary[key] = value;
+        }
+    }
+}
